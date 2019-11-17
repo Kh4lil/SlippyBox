f9447fff25bd6f579c77645ef63e0b147e000e5a
Khalil <khalil.oumimoun@gmail.com>
2019-11-16 23:51:51 -0600

    Initial Level Design

---
+++ SlippyBox/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Reference the component Rigidbody.
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame, we use Time.deltaTime to make it even.
+    // FixedUpdate used for physics.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // adding a forward force.
+
+        //player control
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
